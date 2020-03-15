b0f79b188d3bea628a27cdb1153131eaa9857bd2
Habeeb Okunade <guessy2k2@yahoo.com>
2020-03-15 20:02:20 +1100

    API Documentation with Swagger2

--- api/src/main/java/com/habeebcycle/microservices/api/composite/product/ProductCompositeService.java
+++ api/src/main/java/com/habeebcycle/microservices/api/composite/product/ProductCompositeService.java
@@ -1,8 +1,13 @@
 package com.habeebcycle.microservices.api.composite.product;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+@Api(description = "REST API for composite product information.")
 public interface ProductCompositeService {
     /**
      * Sample usage: curl $HOST:$PORT/product-composite/1
@@ -10,6 +15,15 @@ public interface ProductCompositeService {
      * @param productId the id of product requested
      * @return the composite product info, if found, else null
      */
+    @ApiOperation(
+            value = "${api.product-composite.get-composite-product.description}",
+            notes = "${api.product-composite.get-composite-product.notes}")
+    @ApiResponses(value = {
+            @ApiResponse(code = 400, message = "Bad Request, invalid format of the request. See response message for more information."),
+            @ApiResponse(code = 404, message = "Not found, the specified id does not exist."),
+            @ApiResponse(code = 422, message = "Unprocessable entity, input parameters caused the processing to fails. See response message for more information."),
+            @ApiResponse(code = 500, message = "Internal Server Error, server processing to fails. See response message for more information.")
+    })
     @GetMapping(
             value    = "/product-composite/{productId}",
             produces = "application/json")
--- services/product-composite-service/src/main/java/com/habeebcycle/microservices/services/productcompositeservice/ProductCompositeServiceApplication.java
+++ services/product-composite-service/src/main/java/com/habeebcycle/microservices/services/productcompositeservice/ProductCompositeServiceApplication.java
@@ -1,14 +1,62 @@
 package com.habeebcycle.microservices.services.productcompositeservice;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.client.RestTemplate;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import static java.util.Collections.emptyList;
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
+import static springfox.documentation.builders.RequestHandlerSelectors.basePackage;
+import static springfox.documentation.spi.DocumentationType.SWAGGER_2;
 
 @SpringBootApplication
+@EnableSwagger2
 @ComponentScan("com.habeebcycle")
 public class ProductCompositeServiceApplication {
+
+    @Value("${api.common.version}")           String apiVersion;
+    @Value("${api.common.title}")             String apiTitle;
+    @Value("${api.common.description}")       String apiDescription;
+    @Value("${api.common.termsOfServiceUrl}") String apiTermsOfServiceUrl;
+    @Value("${api.common.license}")           String apiLicense;
+    @Value("${api.common.licenseUrl}")        String apiLicenseUrl;
+    @Value("${api.common.contact.name}")      String apiContactName;
+    @Value("${api.common.contact.url}")       String apiContactUrl;
+    @Value("${api.common.contact.email}")     String apiContactEmail;
+
+    /**
+     * Will exposed on $HOST:$PORT/swagger-ui.html
+     *
+     * @return
+     */
+    @Bean
+    public Docket apiDocumentation() {
+
+        return new Docket(SWAGGER_2)
+                .select()
+                .apis(basePackage("com.habeebcycle.microservices.services.productcompositeservice"))
+                .paths(PathSelectors.any())
+                .build()
+                .globalResponseMessage(GET, emptyList())
+                .apiInfo(new ApiInfo(
+                        apiTitle,
+                        apiDescription,
+                        apiVersion,
+                        apiTermsOfServiceUrl,
+                        new Contact(apiContactName, apiContactUrl, apiContactEmail),
+                        apiLicense,
+                        apiLicenseUrl,
+                        emptyList()
+                ));
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(ProductCompositeServiceApplication.class, args);
